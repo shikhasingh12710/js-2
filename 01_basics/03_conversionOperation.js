e8f2c27d1a5bdfdbf1687450e39c0c3fb3a1c634
shikhasingh12710 <shikhasingh9138@gmail.com>
2025-04-15 05:02:35 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score="shikha"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33"=>33
+//""33ru=>NaN
+//true=>1 false=>0
+//""=>false
+//"shikha"=>true
+
+let isLoggeIn=1;
+let booleanIsLoggedIn=Boolean(isLoggeIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber=22;
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber);
+
+
