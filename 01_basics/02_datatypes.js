597a4d0f070aa7a39a6376a93989ac0b792eaaf8
shikhasingh12710 <shikhasingh9138@gmail.com>
2025-04-15 04:48:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// alert("hello") // we are using nodejs not browser
+
+console.log(3+3)
+console.log("shikha")
+
+let name="Shikha"
+let age=21
+let isLoggedIn=false
+
+//number
+//bigint
+//string=""
+//boolean=true/false
+//null=>standalone value
+//undefined
+//symbol=>unique
+
+//object
+// null is an object
+console.log(typeof undefined);
