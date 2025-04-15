b0a5a5b396656ba10b62f46b83837dc554c5ecd2
shikhasingh12710 <shikhasingh9138@gmail.com>
2025-04-15 11:46:12 +0000

    Conversion and Comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,4 +20,34 @@ let someNumber=22;
 let stringNumber=String(someNumber)
 console.log(typeof stringNumber);
 
+//************************OPERATIONS*******************//
+
+let value=3;
+let negValue=-value;
+console.log(negValue);
+
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log("1"+"2");
+
+// console.log(3+4*5%3);
+
+console.log(+true);
+// console.log(true+);//error
+console.log(+"");
+
+let num1,num2,num3;
+num1=num2=num3=2+2
+
+let gameCounter=100;
+// gameCounter++;
+++gameCounter;
+
+console.log(gameCounter);
+
+
+
+
+
 
